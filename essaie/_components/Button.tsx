b639c49ec7cf646abc57559857a241cc6788c50e
BitdevsGoma <hakizimanazidane@gmail.com>
2025-06-20 11:23:36 +0200

    3rd commit

--- essaie/_components/Button.tsx
+++ essaie/_components/Button.tsx
@@ -2,14 +2,14 @@
 
 import React from "react";
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+type ButtonProps =  React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
 };
 
-export default function Button({ children, className = "", ...props }: ButtonProps) {
+export default function Button({ children, className = "bg-orange-400", ...props }: ButtonProps) {
   return (
     <button
-      className={`px-4 py-2 rounded-md font-semibold text-white bg-green-600 hover:bg-green-700 transition-colors duration-200 ${className}`}
+      className={`px-4 py-2 rounded-full font-semibold text-white bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 hover:bg-green-700 transition-colors duration-200 ${className}`}
       {...props}
     >
       {children}
